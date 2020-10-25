5a3fb7f4b54817241686aaeeccacd6ae99ef6be9
samjwu <swm1000@hotmail.com>
2020-10-25 10:28:54 -0600

    declare winmain

---
+++ Main.cpp
@@ -0,0 +1,19 @@
+#include "Logic.h"
+
+/**
+ * Call WinMain as entry point to start process for Windows application
+ * @param hInstance:
+ *      base address of the memory image of the executable;
+ *      primarily used to load resources from the executable
+ * @param hPrevInstance:
+ *      always set to 0
+ * @param lpCmdLine:
+ *      command line arguments following the command used to start the process
+ * @param nCmdShow:
+ *      controls how the window is to be shown;
+ *      parameter values: 
+ *      https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-showwindow
+ */     
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+
+}
