539d70c16ca91d7a049000f0e2388d384439c492
samjwu <swm1000@hotmail.com>
2020-10-24 19:07:52 -0600

    render init/clear

---
+++ RenderingEngine.cpp
@@ -0,0 +1,23 @@
+#include "RenderingEngine.h"
+
+static SDL_Surface *screen;
+static Uint32 colors[4] = {
+    0x000000ff, // black
+    0xffffffff, // white
+    0x0000ffff, // blue
+    0xff0000ff // red
+};
+
+/*
+ * Constructor. Initializes grid.
+ */
+RenderingEngine::RenderingEngine() {
+	render_grid();
+}
+
+/*
+ * Clear screen by setting all pixels to black
+ */
+void RenderingEngine::clear_screen() {
+	boxColor(screen, 0, 0, screen->w - 1, screen->h - 1, colors[Color::BLACK]);
+}
--- RenderingEngine.h
+++ RenderingEngine.h
@@ -9,14 +9,14 @@ enum class Color {BLACK, WHITE, BLUE, RED};
 class RenderingEngine {
     public:
         RenderingEngine();
+        void render_grid();
+        void clear_screen();
+        void update_screen();
+        void render_tile(int x1, int y1, int x2, int y2, Color tile_color);
+        int get_screen_height();
         int poll_key_input();
         int get_key_input();
         int get_key_state(int key);
-        int get_screen_height();
-        void render_grid();
-        void update_screen();
-        void clear_screen();
-        void render_tile(int x1, int y1, int x2, int y2, Color tile_color);
 };
  
 #endif // _RENDERINGENGINE_
