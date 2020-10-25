c0fc13ceef97e2020b6e5f1ac47c9cadeba1f456
samjwu <swm1000@hotmail.com>
2020-10-25 10:02:19 -0600

    render grid method

--- RenderingEngine.cpp
+++ RenderingEngine.cpp
@@ -16,13 +16,6 @@ RenderingEngine::RenderingEngine() {
 }
 
 /*
- * Render the grid
- */
-void RenderingEngine::render_grid() {
-	
-}
-
-/*
  * Clear screen by setting all pixels to black
  */
 void RenderingEngine::clear_screen() {
@@ -43,6 +36,42 @@ void RenderingEngine::update_screen() {
 */
 void RenderingEngine::render_tile(int x1, int y1, int x2, int y2, Color tile_color) {
     boxColor(screen, x1, y1, x2, y2 - 1, colors[(int) tile_color]);
+}
+
+/*
+ * Render the grid
+ * Return:
+ *      0 if successful
+ *      1 if SDL failed to initialize
+ *      2 if video mode could not be set
+ */
+int RenderingEngine::render_grid() {
+	const SDL_VideoInfo *video_info;
+	Uint8  video_bits_per_pixel;
+	Uint32 video_flags;
+        
+	// initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		fprintf(stderr, "Error during SDL initialization: %s\n", SDL_GetError());
+		return 1;
+	}
+	atexit(SDL_Quit);
+
+    // set video bits per pixel above 8-bit color for better alpha blending
+	video_info = SDL_GetVideoInfo();
+	if (video_info->vfmt->BitsPerPixel > 8) {
+		video_bits_per_pixel = video_info->vfmt->BitsPerPixel;
+	} else {
+		video_bits_per_pixel = 16;
+	}
+	video_flags = SDL_SWSURFACE | SDL_DOUBLEBUF;
+	
+	// set video mode to 640x480
+	if ((screen = SDL_SetVideoMode(640, 480, video_bits_per_pixel, video_flags)) == NULL) {
+		fprintf(stderr, "Couldn't set video mode to %ix%i: %s\n", 640, 480, SDL_GetError());
+		return 2;
+	}
+    return 0;
 }
 
 /* 
--- RenderingEngine.h
+++ RenderingEngine.h
@@ -9,10 +9,10 @@ enum class Color {BLACK, WHITE, BLUE, RED};
 class RenderingEngine {
     public:
         RenderingEngine();
-        void render_grid();
         void clear_screen();
         void update_screen();
         void render_tile(int x1, int y1, int x2, int y2, Color tile_color);
+        int render_grid();
         int get_screen_height();
         int poll_key_input();
         int get_key_input();
