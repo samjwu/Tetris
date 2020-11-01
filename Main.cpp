b89451c2b3ab9e789af06130abc974b27fbc5e0b
samjwu <swm1000@hotmail.com>
2020-11-01 09:30:12 -0700

    advance game logic

--- Main.cpp
+++ Main.cpp
@@ -30,7 +30,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     Logic game_logic(&grid, &tetrominos, &rendering_engine, screen_height);
 
     // ticks time in milliseconds
-    unsigned long ticks_time = SDL_GetTicks();
+    unsigned long ticks_time_1 = SDL_GetTicks();
 
     while (rendering_engine.get_key_state(SDLK_ESCAPE) == 0) {
         // init game screen and starting game logic objects
@@ -90,6 +90,42 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
                     game_logic.current_tetromino_rotation = (game_logic.current_tetromino_rotation + 1) % 4; // 4 possible rotations
                 }
                 break;
+        }
+
+        // advance game by one tick
+        unsigned long ticks_time_2 = SDL_GetTicks();
+        if ((ticks_time_2 - ticks_time_1) > DELAY) {
+            if (grid.tetromino_can_move(game_logic.current_tetromino_x_pos, game_logic.current_tetromino_y_pos + 1, game_logic.current_tetromino_shape, game_logic.current_tetromino_rotation)) {
+                game_logic.current_tetromino_y_pos++;
+            } else {
+                grid.place_tetromino(game_logic.current_tetromino_x_pos, game_logic.current_tetromino_y_pos, game_logic.current_tetromino_shape, game_logic.current_tetromino_rotation);
+                grid.clear_full_lines();
+
+                if (grid.game_over()) {
+                    int buttonId;
+                    if (SDL_ShowMessageBox(&MessageBoxData, &buttonId) < 0) {
+                        SDL_Log("Error displaying message box");
+                        return 1;
+                    }
+                    if (buttonId == -1) {
+                        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Selection Error", "Nothing was selected.", NULL);
+                    } else if (buttonId == 0) {
+                        // create new active instance of application
+                        if (!RA_ActivateRestartProcess()) {
+                            SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Restart Failed", "Error occurred during restart. Exiting application.", NULL);
+                        }
+                    } else { // buttonId == 1
+                        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Game Over", "Ending game.", NULL);
+                    }
+                    RA_DoRestartProcessFinish(); // release mutex
+                    exit(0); // close current instance
+                }
+
+                game_logic.create_new_tetromino(); // generate random next tetromino
+                
+            }
+            
+            ticks_time_1 = SDL_GetTicks();
         }
     }
 
